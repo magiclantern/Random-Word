d0318d4205a7ded5abc32161e47eccdafdf9add1
Nikita U <nikita.u@me.com>
2015-07-17 09:07:51 -0400

    Initial Commit

---
+++ Random Noun/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Random Noun
+//
+//  Created by Nikita U on 2015-07-17.
+//  Copyright (c) 2015 Magic Lantern. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
